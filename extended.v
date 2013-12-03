93ef2b4874c59b090008d0fd5f64d9c62fe5cc72
Wouter Geraedts <git@woutergeraedts.nl>
2013-12-03 15:41:13 +0100

    Completed prelim completeness proof

--- extended.v
+++ extended.v
@@ -29,7 +29,7 @@ Inductive term : Set :=
 | app_term : term -> term -> term
 .
 
-Definition context := var -> option type.
+Definition context := list (var * type).
 
 Fixpoint assoc {A B : Set}
   (A_dec : forall x y : A, {x = y} + {x <> y})
@@ -53,8 +53,8 @@ match l with
 end.
 
 Definition sig_assoc_cons {A B : Set}
-  (A_dec : forall x y : A, {x = y} + {x <> y})
-  (B_dec : forall x y : B, {x = y} + {x <> y})
+  {A_dec : forall x y : A, {x = y} + {x <> y}}
+  {B_dec : forall x y : B, {x = y} + {x <> y}}
   (x : A) (y : B)
   (ls : list (A * B))
   (a : A)
@@ -72,8 +72,8 @@ exact Hb.
 Qed.
 
 Definition neg_assoc_cons {A B : Set} {a : A} {b : B}
-  (A_dec : forall x y : A, {x = y} + {x <> y})
-  (B_dec : forall x y : B, {x = y} + {x <> y})
+  {A_dec : forall x y : A, {x = y} + {x <> y}}
+  {B_dec : forall x y : B, {x = y} + {x <> y}}
   (x : A) (y : B)
   (ls : list (A * B))
   (p : ~assoc A_dec B_dec ls a b)
@@ -101,6 +101,39 @@ intro.
 contradiction.
 Qed.
 
+Definition assoc_B_unique {A B : Set} {l : list (A * B)} {a : A} {b1 b2 : B}
+  {A_dec : forall x y : A, {x = y} + {x <> y}}
+  {B_dec : forall x y : B, {x = y} + {x <> y}}
+  (P1 : assoc A_dec B_dec l a b1)
+  (P2 : assoc A_dec B_dec l a b2)
+  : b1 = b2.
+Proof.
+
+induction l.
+
+unfold assoc in *.
+contradiction.
+
+destruct a0 as (x, y).
+
+unfold assoc in P1.
+unfold assoc in P2.
+destruct (A_dec x a).
+destruct (B_dec b1 y).
+destruct (B_dec b2 y).
+
+rewrite e0.
+rewrite e1.
+reflexivity.
+
+contradiction.
+contradiction.
+
+apply IHl.
+exact P1.
+exact P2.
+Defined.
+
 Fixpoint lookup {A B : Set}
   (A_dec : forall x y : A, {x = y} + {x <> y})
   (B_dec : forall x y : B, {x = y} + {x <> y})
@@ -113,43 +146,88 @@ match l as l0 return {b : B | assoc A_dec B_dec l0 a b} + {forall b : B, ~assoc 
 | nil => inright {b : B | assoc A_dec B_dec nil a b} (fun b : B => assoc_nil)
 | (x, y) :: ls =>
   match A_dec x a with
-  | left eq => inleft (forall b : B, ~ assoc A_dec B_dec ((x, y) :: ls) a b)
-                   (exist (fun b : B => assoc A_dec B_dec ((x, y) :: ls) a b) y
-                      (eq_ind x (fun a0 : A => assoc A_dec B_dec ((x, y) :: ls) a0 y)
-                         (in_eq (x, y) ls) a eq))
-  | right neq => match lookup A_dec B_dec ls a with
-    | inleft result => inleft (forall b : B, ~ assoc ((x, y) :: ls) a b)
-                       (let (b, Hb) := result in
-                        exist (fun b0 : B => In (a, b0) ((x, y) :: ls)) b
-                          (in_cons (x, y) (a, b) ls Hb))
-    | inright result => inright {b : B | assoc ((x, y) :: ls) a b}
-                       (fun b : B => neg_assoc_cons x y ls (result b) neq)
+  | left eq => _
+  | right neq => match lookup A B A_dec B_dec ls a with
+    | inleft result => inleft
+      (forall b : B, ~ assoc A_dec B_dec ((x, y) :: ls) a b)
+      (sig_assoc_cons x y ls a result neq)
+    | inright result => inright
+      {b : B | assoc A_dec B_dec ((x, y) :: ls) a b}
+      (fun b : B => neg_assoc_cons x y ls (result b) neq)
     end
   end
 end.
 
+apply inleft.
+exists y.
+unfold assoc.
+destruct (A_dec x a).
+destruct (B_dec y y).
+exact I.
+elim n.
+reflexivity.
+elim n.
+exact eq.
+
+Defined.
+
 Inductive has_type (g : context) : term -> type -> Prop :=
 | var_has_type : forall v : var, forall A : type,
-  assoc g v A -> has_type g (var_term v) A
+  assoc var_dec type_dec g v A -> has_type g (var_term v) A
 | abs_has_type : forall v : var, forall M : term, forall A B : type,
   has_type ((v, A) :: g) M B -> has_type g (abs_term (v, A) M) (fun_type A B)
-| app_has_type : forall M N : term, forall A B : type,
-  has_type g M (fun_type A B) -> has_type g N A -> has_type g (app_term M N) B
+| app_has_type : forall M N : term, forall B : type, (exists A : type,
+  has_type g M (fun_type A B) /\ has_type g N A) -> has_type g (app_term M N) B
 .
 
-Definition discriminate_has_type {g : context} {T : term} {A B : type} {v : var}
+Fixpoint term_has_unique_type {g : context} {T : term} {A B : type}
+  (P : has_type g T A)
+  (Q : has_type g T B)
+  : A = B.
+Proof.
+destruct T.
+
+inversion_clear P.
+inversion_clear Q.
+rewrite (assoc_B_unique H H0).
+reflexivity.
+
+destruct p as (v, C).
+inversion_clear P.
+inversion_clear Q.
+rewrite (term_has_unique_type ((v, C) :: g) T B0 B1 H H0).
+reflexivity.
+
+inversion_clear P.
+inversion_clear Q.
+
+destruct H.
+destruct H0.
+destruct H.
+destruct H0.
+rewrite (term_has_unique_type g T2 x x0 H1 H2) in H.
+assert (fun_type x0 A = fun_type x0 B).
+apply (term_has_unique_type g T1 (fun_type x0 A) (fun_type x0 B) H H0).
+inversion_clear H3.
+reflexivity.
+Qed.
+
+
+Lemma type_inconsistency {g : context} {T : term} {A B : type} {v : var}
   (P : has_type g T (fun_type A B))
   (Q : has_type g T (var_type v))
   : False.
 Proof.
-destruct T.
-inversion_clear Q.
-inversion_clear P.
-unfold assoc in H0.
-unfold assoc in H.
 
+elim (type_dec (fun_type A B) (var_type v)).
 
+intro.
+inversion a.
 
+intro.
+elim b.
+apply (term_has_unique_type P Q).
+Qed.
 
 Fixpoint type_check (g : context) (T : term) {struct T}
  : {A : type | has_type g T A} + {forall A : type, ~has_type g T A}.
@@ -173,7 +251,7 @@ refine
    end
  end.
 
-destruct (lookup var_dec g v).
+destruct (lookup var_dec type_dec g v).
 apply inleft.
 destruct s.
 exists x.
@@ -183,9 +261,9 @@ exact a.
 apply inright.
 intro.
 intro.
-inversion H.
+inversion_clear H.
 apply (n A).
-exact H1.
+exact H0.
 
 destruct (type_check ((v, A) :: g) M).
 apply inleft.
@@ -194,31 +272,63 @@ exists (fun_type A B).
 apply (abs_has_type g v M A B).
 exact HB.
 
-Show Proof.
+apply inright.
+intro B.
+intro.
+inversion_clear H.
+apply (n B0).
+exact H0.
+
+apply inright.
+intro.
+intro.
+inversion_clear H.
+destruct H0.
+destruct H.
+apply (type_inconsistency H _H).
+
+apply inleft.
+exists CB.
+apply app_has_type.
+exists CA.
+split.
+exact HCf.
+rewrite <- eq in HA.
+exact HA.
+
+apply inright.
+intro CB0.
+intro.
+
+inversion_clear H.
+destruct H0.
+destruct H.
+rewrite (term_has_unique_type H0 HA) in H.
+elim n.
+assert (fun_type CA CB = fun_type A CB0).
+apply (term_has_unique_type HCf H).
+inversion_clear H1.
+reflexivity.
 
 apply inright.
 intro B.
 intro.
-inversion H.
-apply (n B0).
-exact H4.
+inversion_clear H.
+destruct H0.
+destruct H.
+destruct (n x).
+exact H0.
 
 apply inright.
 intro.
 intro.
-inversion H.
-inversion _H.
+inversion_clear H.
+destruct H0.
+destruct H.
+destruct (n (fun_type x A)).
+exact H.
 
-Print has_type_ind.
-
-(*intro.
-intro.
-inversion H.*)
-
-
-
-
-
+Defined.
 
 Definition gamma := cons ("x", var_type "a") (cons ("n", var_type "Nat") nil).
 
